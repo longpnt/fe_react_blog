fa97b94b7540bac469ec8a1f456f88c173077bc9
longpnt <longpnt01@gmail.com>
2021-12-25 16:58:16 +0700

    hihi

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import {ContextProvider} from "./context/Context";
+import { ContextProvider } from "./context/Context";
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
       <App />
     </ContextProvider>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById("root"));
+const PORT = process.env.development || 3000;
+const HOST = "https://react-deloy.herokuapp.com/";
+
